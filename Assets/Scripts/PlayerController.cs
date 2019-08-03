d2e22882eb7905373d108cede07d34f2872cce13
NekitMalyarenko <n.a.m.62608@gmail.com>
2019-08-03 20:51:55 +0300

    basic player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey("w"))
+        {
+            movement.z = 1.0f;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            movement.z = -1.0f;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            movement.x = 1.0f;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            movement.x = -1.0f;
+        }
+
+
+        if (movement != Vector3.zero)
+        {
+            //print(rb.velocity);
+            rb.velocity = movement * speed;
+        }
+    }
+}
